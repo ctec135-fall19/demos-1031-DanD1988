82dad94e1ec653bc6dbc8d4ff7ab6c593d7cbb30
Daniel Davidson <DanDavidson88@gmail.com>
2019-10-31 15:14:10 -0700

    Demo 1031

---
+++ Demo1031Scary/MyMath1.UnitTest/MyMath1Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace MyMath1.UnitTest
+{
+    class MyMath1Tests
+    {
+        [Test]
+
+        public void Add_SumWithInByteRange
+        {
+            Assert.AreEqual(200,MyMath.MyMath1.Add(101, 99));
+        }
+
+        [Test] 
+        public void Add_sumOutsinByteRange_AlternateAssert()
+    {
+        Assert.AreEqual(200, MyMath.MyMath1.Add(101, 99));
+    }
+    }
+}
+}
